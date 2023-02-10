454f169bfaaa0692f5e660e5e029d81a013a31df
Jordan Wolter <jwolter456@gmail.com>
2023-02-10 17:38:20 -0500

    testing commit

---
+++ Big0.js
@@ -0,0 +1,17 @@
+
+/*
+What is good code?
+
+    Code that is Readable
+    Code that is Scalable
+
+
+*/
+
+const nemo = ['nemo'];
+
+function findNemo(array) {
+    for (let i = 0; i <array.length; i++){
+        
+    }
+}

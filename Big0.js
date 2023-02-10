0c4754f19cb4fc84870febae265f00ae7f2ecf1d
Jordan Wolter <jwolter456@gmail.com>
2023-02-10 17:53:13 -0500

    messing around with runtime

--- Big0.js
+++ Big0.js
@@ -9,9 +9,22 @@ What is good code?
 */
 
 const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+const large = new Array(100).fill('nemo');
 
 function findNemo(array) {
+    let t0 = performance.now()
     for (let i = 0; i <array.length; i++){
-        
+        if (array[i] === 'nemo'){
+            console.log('Found NEMO!');
+        }
     }
+    let t1 = performance.now()
+    console.log("Call to find nemo took " + (t1-t0) + " milliseconds")
 }
+
+findNemo(large)
+
+// How long does it take to run a problem or task = runtime
+// Can use performance.now to check 
+// from 1 to 10 not a big change but as our inputs icrease our runtime increases (becomes slower)

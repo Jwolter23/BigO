7caa4b71a40e112488602126e95a2f590d6a2c31
Jordan Wolter <jwolter456@gmail.com>
2023-02-10 18:16:28 -0500

    BigO complexity chart

--- Big0.js
+++ Big0.js
@@ -13,13 +13,13 @@ const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', '
 const large = new Array(100).fill('nemo');
 
 function findNemo(array) {
-    let t0 = performance.now()
+    // let t0 = performance.now()
     for (let i = 0; i <array.length; i++){
         if (array[i] === 'nemo'){
             console.log('Found NEMO!');
         }
     }
-    let t1 = performance.now()
+    // let t1 = performance.now()
     console.log("Call to find nemo took " + (t1-t0) + " milliseconds")
 }
 
@@ -28,3 +28,14 @@ findNemo(large)
 // How long does it take to run a problem or task = runtime
 // Can use performance.now to check 
 // from 1 to 10 not a big change but as our inputs icrease our runtime increases (becomes slower)
+
+/*
+BIG O COMPLEXITY CHART
+
+Horrible: O(n!), O(2^n), O(n^2)
+Bad: O(n log n)
+fair: O(n)
+Good / Excellent: O(log n), O(1)
+
+*/
+
